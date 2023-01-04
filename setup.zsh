c692b17f4c8bda947886ff0e18e5689a9bf99978
Jure Glasovic <jglasovic@gmail.com>
2023-01-04 16:26:20 +0100

    better setup script

--- setup.zsh
+++ setup.zsh
@@ -1,4 +1,13 @@
 #!/bin/zsh
+ 
+SELF_PATH="$( cd "$( dirname "$0" )" && pwd )"
+
+# source utils 
+. "$SELF_PATH/zsh.symlink/utils.sh"
+
+if check_file_exists "$HOME/.profile" ; then
+  rm -r "$HOME/.profile"
+fi
 
 source "$HOME/.zshrc"
 
@@ -6,9 +15,21 @@ source "$HOME/.zshrc"
 py_3=3.9.9
 py_2=2.7.11
 py_version=$(pyenv version)
+py_versions=$(pyenv versions)
+
+if [[ "$py_versions" != *"$py_3"* ]]; then
+  echo "installing py version $py_3"
+  pyenv install $py_3
+fi
+
+if [[ "$py_versions" != *"$py_2"* ]]; then
+  echo "installing py version $py_2"
+  pyenv install $py_2
+fi
 
 if [[ "$py_version" != *"$py_3"* ]]; then
-  pyenv install $py_3
+  echo "current py version is set to $py_version"
+  echo "setting global py version to $py_3"
   pyenv global $py_3
 fi
 
@@ -19,24 +40,25 @@ if ! command_exists "poetry" ; then
   echo "poetry has been successfully instaled!"
 fi
 
-# setup nvm
-# add yarn, ts-node, neovim to be installed global for all node versions
-# echo "yarn\nts-node\nneovim" > "$HOME/.nvm/default-packages"
-# nvm install --lts
-# nvm alias default 'lts/*'
-
 # setup neovim py
 if ! check_file_exists "$HOME/.pyenv/versions/neovim2/bin/python" ; then
-  pyenv install $py_2
   pyenv virtualenv $py_2 neovim2
   pyenv activate neovim2
   pip install neovim
   pyenv deactivate neovim2
 fi
-
 if ! check_file_exists "$HOME/.pyenv/versions/neovim3/bin/python" ; then
   pyenv virtualenv $py_3 neovim3
   pyenv activate neovim3
   pip install neovim
   pyenv deactivate neovim3
 fi
+
+# setup nvm
+# add yarn, ts-node, neovim to be installed global for all node versions
+echo "yarn\nts-node\nneovim" > "$HOME/.nvm/default-packages"
+nvm use default &> /dev/null
+if [ $? != 0 ] ; then
+  nvm install --lts --default
+fi
+
