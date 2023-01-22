4795afc6d539ea158c3b1d10c46403fe233004cb
Jure Glasovic <jglasovic@gmail.com>
2023-01-22 22:10:12 +0100

    fix tmux restore script

--- .zsh/config/tmux-sessions-restore.sh
+++ .zsh/config/tmux-sessions-restore.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 
 # require tmux config json file
 function tmux_restore(){
@@ -21,21 +21,21 @@ function tmux_restore(){
     for window_encoded in $session_windows; do
       local window_decoded=`echo $window_encoded | base64 --decode`
       local window_name=`echo $window_decoded | jq -r ".name"`
-      local window_path=`echo $window_decoded | jq -r ".path" | tr -d \'\"`
-      echo $window_path
+      local window_path=`echo "$window_decoded" | jq -r ".path"`
+      window_path=`eval "echo $window_path"`
 
       # if session doesn't exist, create it with a first window
       if [[ $has_session != 0 ]]; then
-        tmux new-session -d -s $session_name -n $window_name -c $window_path
+        tmux new-session -d -s "$session_name" -n "$window_name" -c "$window_path"
 
         has_session=0
         continue
       fi
 
       # if window doesn't exist, create it
-      tmux has_session -t $session_name:$window_name &> /dev/null
+      tmux has-session -t "$session_name:$window_name" &> /dev/null
       if [[ $? != 0 ]]; then
-        tmux new-window -a -n $window_name -t $session_name -c $window_path
+        tmux new-window -a -n "$window_name" -t "$session_name" -c "$window_path"
       fi
 
     done
@@ -47,6 +47,6 @@ function tmux_restore(){
     local session_decoded=`echo $last_session | base64 --decode`
     local last_session_name=`echo $session_decoded | jq -r ".name"`
 
-    tmux attach -t $last_session_name
+    tmux attach -t "$last_session_name"
   fi
  }
