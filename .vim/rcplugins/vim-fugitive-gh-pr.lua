48fc5447db094c5e5d156c864aa3a1160379e56c
Jure Glasovic <jglasovic@gmail.com>
2024-05-13 11:15:56 +0200

    remove callbacks and use coroutines

--- .vim/rcplugins/vim-fugitive-gh-pr.lua
+++ .vim/rcplugins/vim-fugitive-gh-pr.lua
@@ -2,86 +2,122 @@ if not vim.fn.executable('gh') then
   return
 end
 
-local exec_wrapper = function(cmd, args, callback)
-  return ExecuteShellCommandAsync(cmd, args,
-    function(code, signal, stdout_output, stderr_output)
-      if code == 0 then
-        return callback(nil, stdout_output)
-      else
-        return callback(true, stderr_output)
-      end
+-- could use plenary.nvim but decided to go with custom coroutines in lua
+local uv = vim.loop
+
+local execute_command = function(command)
+  local co = coroutine.running()
+  local stdout = uv.new_pipe()
+  local stderr = uv.new_pipe()
+  local handle
+  local stdout_output = {}
+  local stderr_output = {}
+
+  handle = uv.spawn('sh', {
+    args = {'-c', command},
+    stdio = { nil, stdout, stderr },
+  }, function(code, _)
+    stdout:read_stop()
+    stderr:read_stop()
+    stdout:close()
+    stderr:close()
+    handle:close()
+
+    if code == 0 then
+      coroutine.resume(co, {nil, table.concat(stdout_output)})
+    else
+      coroutine.resume(co, {true, table.concat(stderr_output)})
     end
-  )
-end
+  end)
 
-local sync_base_branch = function(base_ref, callback)
-  return exec_wrapper('git', { 'fetch', 'origin', base_ref },
-    function(err)
-      if not err then
-        return callback(nil, base_ref)
-      end
-      return exec_wrapper('gh', { 'repo', 'view', '--json=defaultBranchRef' },
-        function(err, default_branch)
-          if err then
-            return callback(true, "Cannot find base branch origin " .. base_ref)
-          end
-          -- TODO: parse | jq -r '.defaultBranchRef.name' and use name
-          return exec_wrapper('git', { 'fetch', 'origin', default_branch },
-            function(err)
-              if err then
-                callback(true, "Error fetching origin " .. default_branch)
-              end
-              return callback(nil, default_branch)
-            end)
-        end)
-    end)
+  uv.read_start(stdout, function(err, data)
+    assert(not err, err)
+    if data then
+      table.insert(stdout_output, data)
+    end
+  end)
+
+  uv.read_start(stderr, function(err, data)
+    assert(not err, err)
+    if data then
+      table.insert(stderr_output, data)
+    end
+  end)
+  local result = coroutine.yield()
+  return result[1], result[2]
 end
 
 
-local sync_ref_branch = function(head_ref, pr_number, callback)
-  return exec_wrapper('git', { 'fetch', 'origin', head_ref },
-    function(err)
-      if not err then
-        return callback(nil, { head_ref, 'origin/' .. head_ref })
-      end
-      return exec_wrapper('git', { 'fetch', 'origin', 'pull/' .. pr_number .. '/head:' .. head_ref },
-        function(err)
-          if err then
-            return callback(true, 'Cannot fetch origin ' .. head_ref)
-          end
-          return callback(nil, { head_ref, head_ref })
-        end)
-    end)
+local sync_base_branch = function(base_ref)
+  local err, _ = execute_command('git fetch origin '..base_ref)
+  if not err then
+    return nil, base_ref
+  end
+  local format="--jq='.defaultBranchRef.name'"
+  local cmd ='gh repo view --json=defaultBranchRef '..format
+  local err, default_branch = execute_command(cmd)
+  if err then
+    return true, "Cannot find base branch origin " .. base_ref
+  end
+  local err, _ = execute_command('git fetch origin'..default_branch)
+  if err then
+    return true, "Error fetching origin " .. default_branch
+  end
+  return nil, default_branch
 end
 
 
-local fetch_pr_data = function(pr_number, callback)
-  return exec_wrapper('gh', { 'pr', 'view', '--json=state,mergeCommit,baseRefName,headRefName', pr_number },
-    function(err, data)
-      if err then
-        return callback(true, data)
-      end
-      local cmd = "jq -r '.baseRefName,.headRefName,.state,.mergeCommit.oid' <<< '" .. data .. "'"
-      return exec_wrapper('sh', { '-c', cmd },
-        function(err, data)
-          if err then
-            return callback(true, data)
-          end
-          local keys = { "baseRefName", "headRefName", "state", "mergeCommit" }
-          local response = { baseRefName = nil, headRefName = nil, state = nil, mergeCommit = nil }
-          local i = 1
-          for value in data:gmatch("[^\r\n]+") do
-            local key = keys[i]
-            if value == "null" then
-              response[key] = nil
-            else
-              response[key] = value
-            end
-            i = i + 1
-          end
-          return callback(nil, response)
-        end)
-    end)
+local sync_ref_branch = function(head_ref, pr_number)
+  local err, _ = execute_command('git fetch origin '..head_ref)
+  if not err then
+    return nil, {head_ref, 'origin/'..head_ref}
+  end
+  local err, _ = execute_command('git fetch origin pull/'..pr_number..'/head:'..head_ref)
+  if err then
+    return true, 'Cannot fetch origin ' .. head_ref
+  end
+  return nil, { head_ref, head_ref }
+end
+
+
+local fetch_pr_data = function(pr_number)
+  local format = "--jq='.baseRefName,.headRefName,.state,.mergeCommit.oid'"
+  local cmd = 'gh pr view '..pr_number..' --json=state,mergeCommit,baseRefName,headRefName '..format
+  local err, output = execute_command(cmd)
+  if err then
+    return true, output
+  end
+  local keys = { "baseRefName", "headRefName", "state", "mergeCommit" }
+  local response = { baseRefName = nil, headRefName = nil, state = nil, mergeCommit = nil }
+  local i = 1
+  for value in output:gmatch("[^\r\n]+") do
+    local key = keys[i]
+    if value == "null" then
+      response[key] = nil
+    else
+      response[key] = value
+    end
+    i = i + 1
+  end
+  return nil, response
+end
+
+local find_merge_base = function(base,head)
+  local err, data = execute_command('git merge-base '..base..' '..head)
+  if err then
+    return true, err
+  end
+
+  local merge_base_commit_sha = nil
+  for value in data:gmatch("[^\r\n]+") do
+    if value then
+      merge_base_commit_sha = value
+    end
+  end
+  if merge_base_commit_sha == nil then
+    return true, "Cannot find merge-base commit"
+  end
+  return nil, merge_base_commit_sha
 end
 
 local run_vim_diff_command = function(from, to)
@@ -91,71 +127,56 @@ local run_vim_diff_command = function(from, to)
   end)
 end
 
+local main = function(pr_number, checkout)
+  print('Fetching PR information for #' .. pr_number)
+  local err, data = fetch_pr_data(pr_number)
+  if err then
+    vim.api.nvim_err_writeln(data)
+    return
+  end
+  local baseRefName = data.baseRefName
+  local headRefName = data.headRefName
+  local state = data.state
+  local mergeCommit = data.mergeCommit
+
+  print('Fetching base branch: ' .. baseRefName)
+  local err, data = sync_base_branch(baseRefName)
+  if err then
+    vim.api.nvim_err_writeln(data)
+    return
+  end
+  if state == 'MERGED' and mergeCommit ~= 'null' then
+    return run_vim_diff_command(mergeCommit .. '^', mergeCommit)
+  end
+
+  print('Fetching ref branch: ' .. headRefName)
+  local err, data = sync_ref_branch(headRefName, pr_number)
+  if err then
+    vim.api.nvim_err_writeln(data)
+    return
+  end
+  local headRefName = data[1]
+  local originHeadRefName = data[2]
+  local err, merge_base_commit_sha = find_merge_base(baseRefName, originHeadRefName)
+
+  if err then
+    vim.api.nvim_err_writeln(data)
+    return
+  end
+
+  if checkout ~= 0 then
+    print("Running checkout on branch " .. headRefName)
+    local err, _ = execute_command('git checkout '..headRefName)
+    if err then
+      vim.api.nvim_err_writeln(data)
+      return
+    end
+    return run_vim_diff_command(merge_base_commit_sha, '')
+  end
+
+  return run_vim_diff_command(merge_base_commit_sha, originHeadRefName)
+end
 
 function GhPr(pr_number, checkout)
-  print('Fetching PR information for #' .. pr_number)
-  fetch_pr_data(pr_number,
-    function(err, data)
-      if err then
-        vim.api.nvim_err_writeln(data)
-        return
-      end
-
-      local baseRefName = data.baseRefName
-      local headRefName = data.headRefName
-      local state = data.state
-      local mergeCommit = data.mergeCommit
-
-      print('Fetching base branch: ' .. baseRefName)
-      return sync_base_branch(baseRefName,
-        function(err, data)
-          if err then
-            vim.api.nvim_err_writeln(data)
-            return
-          end
-          if state == 'MERGED' and mergeCommit ~= 'null' then
-            return run_vim_diff_command(mergeCommit .. '^', mergeCommit)
-          end
-          print('Fetching ref branch: ' .. headRefName)
-          return sync_ref_branch(headRefName, pr_number,
-            function(err, data)
-              if err then
-                vim.api.nvim_err_writeln(data)
-                return
-              end
-              local headRefName = data[1]
-              local originHeadRefName = data[2]
-              return exec_wrapper('git', { 'merge-base', baseRefName, originHeadRefName },
-                function(err, data)
-                  if err then
-                    vim.api.nvim_err_writeln(data)
-                    return
-                  end
-
-                  local merge_base_commit_sha = nil
-                  for value in data:gmatch("[^\r\n]+") do
-                    if value then
-                      merge_base_commit_sha = value
-                    end
-                  end
-                  if merge_base_commit_sha == nil then
-                    vim.api.nvim_err_writeln("Cannot find merge-base commit")
-                    return
-                  end
-                  if checkout ~= 0 then
-                    print("Running checkout on branch " .. headRefName)
-                    return exec_wrapper('git', { 'checkout', headRefName },
-                      function(err, data)
-                        if err then
-                          vim.api.nvim_err_writeln(data)
-                          return
-                        end
-                        return run_vim_diff_command(merge_base_commit_sha, '')
-                      end)
-                  end
-                  return run_vim_diff_command(merge_base_commit_sha, originHeadRefName)
-                end)
-            end)
-        end)
-    end)
+  coroutine.wrap(main)(pr_number, checkout)
 end
